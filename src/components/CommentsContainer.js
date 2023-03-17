20c0cc385fc43f14b155aa14c969cc0b85d98b60
Sourav Nag <sn.sourav1993@gmail.com>
2023-03-17 21:10:04 +0530

    N lable nested comments

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,343 @@
+import React from "react";
+
+const commentData = [
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [
+              {
+                name: "Sourav Nag",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                replies: [
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [
+              {
+                name: "Sourav Nag",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                replies: [
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [
+              {
+                name: "Sourav Nag",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                replies: [
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [
+              {
+                name: "Sourav Nag",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                replies: [
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [
+                      {
+                        name: "Sourav Nag",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        replies: [
+                          {
+                            name: "Sourav Nag",
+                            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                            replies: [
+                              {
+                                name: "Sourav Nag",
+                                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                replies: [
+                                  {
+                                    name: "Sourav Nag",
+                                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                    replies: [
+                                      {
+                                        name: "Sourav Nag",
+                                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                        replies: [
+                                          {
+                                            name: "Sourav Nag",
+                                            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                            replies: [
+                                              {
+                                                name: "Sourav Nag",
+                                                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                replies: [
+                                                  {
+                                                    name: "Sourav Nag",
+                                                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                    replies: [],
+                                                  },
+                                                ],
+                                              },
+                                            ],
+                                          },
+                                        ],
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sourav Nag",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    replies: [
+      {
+        name: "Sourav Nag",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        replies: [
+          {
+            name: "Sourav Nag",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            replies: [
+              {
+                name: "Sourav Nag",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                replies: [
+                  {
+                    name: "Sourav Nag",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    replies: [
+                      {
+                        name: "Sourav Nag",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        replies: [
+                          {
+                            name: "Sourav Nag",
+                            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                            replies: [
+                              {
+                                name: "Sourav Nag",
+                                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                replies: [
+                                  {
+                                    name: "Sourav Nag",
+                                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                    replies: [],
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg m-2">
+      <img
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        alt="user"
+        className="w-12 h-12"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentsList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments:</h1>
+      <CommentsList comments={commentData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
