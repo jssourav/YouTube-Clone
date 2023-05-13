5065a78b436537322b4a86c8240613e15a5582ce
Sourav Nag <sn.sourav1993@gmail.com>
2023-05-13 17:07:11 +0530

    added live chat user input

--- src/components/Livechat.js
+++ src/components/Livechat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
@@ -7,6 +7,7 @@ import { randomNameGenerator, randomStringGenerator } from "../utils/helper";
 // const data
 
 const Livechat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -21,17 +22,44 @@ const Livechat = () => {
           message: randomStringGenerator() + "🚀🚀",
         })
       );
-    }, 500);
+    }, 2000);
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="ml-2 w-full h-[550px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((c, index) => (
-        <ChatMessage key={index} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="ml-2 w-full h-[550px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full p-2 ml-2  border border-black flex rounded-lg"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Sourav Nag",
+              message: liveMessage + "🚀🚀",
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-full focus:outline-none"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-200">Send</button>
+      </form>
+    </>
   );
 };
 
